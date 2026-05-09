c8870a6b7dbd25bdba3d96076c4ce9b80e053c59
DudaAraujo14 <rm560944@fiap.com.br>
2026-05-09 17:13:07 -0300

    feat: implementação do consumer de usuários validados

---
+++ ReceiverUsuarios/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("RECEIVER USUÁRIOS INICIADO...");
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    exchange: "hortifruti.exchange.validated",
+    type: ExchangeType.Direct
+);
+
+await channel.QueueDeclareAsync(
+    queue: "queue.usuarios.receiver",
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+);
+
+await channel.QueueBindAsync(
+    queue: "queue.usuarios.receiver",
+    exchange: "hortifruti.exchange.validated",
+    routingKey: "usuarios.validated"
+);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+
+    var mensagem = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine("\nUSUÁRIO VALIDADO RECEBIDO:");
+    Console.WriteLine(mensagem);
+
+    await Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+    queue: "queue.usuarios.receiver",
+    autoAck: true,
+    consumer: consumer
+);
+
+Console.WriteLine("AGUARDANDO MENSAGENS...");
+Console.ReadLine();
